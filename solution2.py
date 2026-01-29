2ca80aa4337cace22ea5b2da3ae4b54e29b79674
Tapesh Kumar <tapeshkumar1998@gmail.com>
2026-01-29 12:12:11 +0530

    Create solution2.py

---
+++ solution2.py
@@ -0,0 +1,8 @@
+original_list =[1,2,3,4,5,6,7,8,9,10]
+
+first_five_elements = original_list[:5]
+reversed_elements  = first_five_elements[::-1]
+
+print(f"Original list: {original_list}")
+print( f"Extracted first five elements: {first_five_elements}")
+print(f"Reversed extracted elements: {reversed_elements}")
